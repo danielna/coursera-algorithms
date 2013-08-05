e788508778470f4adf3480988028dd449a5d27fe
Daniel Na <daniel.na@gmail.com>
2013-08-05 15:05:37 -0400

    Cleanup

--- merge_sort.js
+++ merge_sort.js
@@ -16,13 +16,9 @@
             return array;
         }
         var arraySplit1 = array.slice(0, length/2),
-            arraySplit2 = array.slice(length/2, array.length);
-
-        // console.log("arraySplit1:", arraySplit1);
-        // console.log("arraySplit2:", arraySplit2);
-
-        var sortedArray1 = mergeSort(arraySplit1);
-        var sortedArray2 = mergeSort(arraySplit2);
+            arraySplit2 = array.slice(length/2, array.length),
+            sortedArray1 = mergeSort(arraySplit1),
+            sortedArray2 = mergeSort(arraySplit2);
 
         mergedArray = mergeArrays(sortedArray1, sortedArray2, length);
         return mergedArray;
@@ -33,16 +29,8 @@
             i = 0,
             j = 0;
 
-        // console.log("MERGE:");
-        // console.log("arr1:", arr1);
-        // console.log("arr2:", arr2);  
-
         for (var n = 0; n < length; n++){
-            // console.log("i:", i);
-            // console.log("j:", j);
-            // console.log("arr[i]:", arr1[i]);
-            // console.log("arr[j]:", arr2[j]);
-            if ((arr1[i] < arr2[j]) || (typeof arr2[j] === "undefined")) {
+            if ((arr1[i] <= arr2[j]) || (typeof arr2[j] === "undefined")) {
                 if (typeof arr1[i] !== "undefined") {
                     mergedArray.push(arr1[i]);
                     i++;
@@ -52,19 +40,11 @@
                     mergedArray.push(arr2[j]);
                     j++;
                 }
-            } else {
-                mergedArray.push(arr1[i]);
-                mergedArray.push(arr2[j]);
-
-                i++;
-                j++;
             }
         }
 
-        // console.log("MERGED:", mergedArray);
         return mergedArray;
     };
-
 
     console.log("initialArray:", array);
     var output = mergeSort(array);
