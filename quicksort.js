55d84f87eb0ac9ab3826ad3a09bf6ccfd5a001b9
Daniel Na <daniel.na@gmail.com>
2013-08-06 20:13:03 -0400

    add quicksort, with a lot of console outputs

---
+++ quicksort.js
@@ -0,0 +1,65 @@
+(function() {
+
+    // Quicksort
+    //
+    // Pick a pivot item (move it to the first item), place items before and after it by swapping
+    // Keep track of two indices as you traverse the array
+    // This is a horrible description
+    //
+    // With random pivot selection, should be O(nlogn)
+    // Worst case pivot selection (first item in an already sorted array) is O(n^2)
+
+    var array = [3,8,2,5,1,4,7,6];
+    // var array = [3,5,2,1,4];
+
+    var quickSort = function(array) {
+        // first element pivot
+        partition(array, 0, array.length);
+        return array;
+    };
+
+    var partition = function(array, leftIndex, rightIndex) {
+        
+        console.log("** RECURSE **");
+        console.log("array:", array);
+        console.log("leftIndex:", leftIndex);
+        console.log("rightIndex:", rightIndex);
+        console.log("************");
+        var hasSwapped = false;
+
+
+        if (leftIndex < rightIndex) {
+            var pivot = array[leftIndex],
+                i = leftIndex + 1;
+
+            for (var j = i; j < rightIndex; j++) {
+                if (array[j] < pivot) {
+                    // swap
+                    var temp = array[j];
+                    array[j] = array[i];
+                    array[i] = temp;
+                    // console.log("swap - j:", array[j], " & i:", array[i]);
+                    // console.log("post-swap array:", array);
+                    i++;
+                    hasSwapped = true;
+                }
+            }
+
+            array[leftIndex] = array[i-1];
+            array[i-1] = pivot;
+
+            console.log("array:", array);
+            if (hasSwapped) {
+                partition(array, 0, i-1);
+                partition(array, i, rightIndex);
+            }
+            
+        } else {
+            return array;
+        }
+    };
+
+    var x = quickSort(array);
+    console.log("quicksorted array:", x);
+
+})();
