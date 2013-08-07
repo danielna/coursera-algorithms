ced57ee0728ff665014da6c55c3cc0b4a88d03cb
Daniel Na <daniel.na@gmail.com>
2013-08-06 22:53:43 -0400

    Fix the quickSort to work properly.

--- quicksort.js
+++ quicksort.js
@@ -9,8 +9,21 @@
     // With random pivot selection, should be O(nlogn)
     // Worst case pivot selection (first item in an already sorted array) is O(n^2) -- very rare
 
-    var array = [3,8,2,5,1,4,7,6];
-    // var array = [3,5,2,1,4];
+    // Load the contents of PS1.txt
+    var xhr = new XMLHttpRequest();
+    xhr.open('GET', '/input/QuickSort.txt', false);
+    xhr.send(null);
+
+    // Convert the STRINGS to INTEGERS
+    var arrayString = xhr.responseText.split('\n');
+    var array = [];
+    for(var x in arrayString) {
+        array.push(parseInt(arrayString[x], 10));
+    }
+
+    var comparisions = 0;
+
+    // var array = [3,8,2,5,1,4,7,6];
 
     var quickSort = function(array) {
         // first element pivot
@@ -20,10 +33,13 @@
 
     var partition = function(array, leftIndex, rightIndex) {
 
-        // Adding this in so the recursion doesn't go farther than it needs to
-        var hasSwapped = false;
+        // console.log("** RECURSE **");
+        // console.log("leftIndex:", leftIndex);
+        // console.log("rightIndex:", rightIndex);
+        // console.log("array subsection:", array.slice(leftIndex, rightIndex));
+        // console.log("************");
 
-        if (leftIndex < rightIndex) {
+        if (leftIndex < rightIndex-1) {
             var pivot = array[leftIndex],
                 i = leftIndex + 1;
 
@@ -33,10 +49,9 @@
                     var temp = array[j];
                     array[j] = array[i];
                     array[i] = temp;
-                    // console.log("swap - j:", array[j], " & i:", array[i]);
+                    // console.log("swap - j:", j, " + a[j]:", array[j], " & i:", i, " + a[i]:", array[i]);
                     // console.log("post-swap array:", array);
                     i++;
-                    hasSwapped = true;
                 }
             }
 
@@ -44,18 +59,18 @@
             array[leftIndex] = array[i-1];
             array[i-1] = pivot;
 
-            // console.log("array:", array);
-            if (hasSwapped) {
-                partition(array, 0, i-1);
-                partition(array, i, rightIndex);
-            }
+            // console.log("array end:", array.slice(leftIndex, rightIndex));
+
+            partition(array, leftIndex, i);
+            partition(array, i, rightIndex);
             
         } else {
-            return array;
+            // console.log("return: ", array.slice(leftIndex, rightIndex), "\n");
+            return array.slice(leftIndex, rightIndex);
         }
     };
 
-    var x = quickSort(array);
-    console.log("quicksorted array:", x);
+    var sorted = quickSort(array);
+    console.log("quicksorted array:", sorted);
 
 })();
