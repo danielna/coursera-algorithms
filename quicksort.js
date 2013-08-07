49579f11279623d8635e642f5d69e16a89c38a47
Daniel Na <daniel.na@gmail.com>
2013-08-07 12:32:43 -0400

    Remove trailing newline, AGAIN.  THINGS WORK NOW

--- quicksort.js
+++ quicksort.js
@@ -9,6 +9,10 @@
     // With random pivot selection, should be O(nlogn)
     // Worst case pivot selection (first item in an already sorted array) is O(n^2) -- very rare
 
+    // 162085 using first as pivot
+    // 164123 using last as pivot
+    // 138382 using median of 3 as pivot    
+
     // Load the contents of PS1.txt
     var xhr = new XMLHttpRequest();
     xhr.open('GET', '/input/QuickSort.txt', false);
@@ -21,19 +25,39 @@
         array.push(parseInt(arrayString[x], 10));
     }
 
-    var comparisions = 0;
+    var comparisons = 0;
 
     // var array = [3,8,2,5,1,4,7,6];
 
     var quickSort = function(array) {
         // first element pivot
+
+        console.log("array:", array);
         partition(array, 0, array.length);
         return array;
     };
 
     var partition = function(array, leftIndex, rightIndex) {
-
+        
         if (leftIndex < rightIndex-1) {
+            comparisons += (rightIndex - 1) - leftIndex;
+
+            // Problem 2 edit:
+            // when making pivot the last item
+            // console.log("\n");
+            // console.log("array", array);
+            
+            // var _temp = array[leftIndex];
+            // array[leftIndex] = array[rightIndex-1];
+            // array[rightIndex-1] = _temp;
+
+            // console.log("SWAP");
+            // console.log("array[leftIndex]", array[leftIndex]);
+            // console.log("array[rightIndex-1]", array[rightIndex-1]);
+            // console.log("array.slice", array.slice(leftIndex, rightIndex));
+
+            // debugger;
+
             var pivot = array[leftIndex],
                 i = leftIndex + 1;
 
@@ -51,15 +75,23 @@
             array[leftIndex] = array[i-1];
             array[i-1] = pivot;
 
-            partition(array, leftIndex, i);
+            // console.log("pivot at " + (i-1) + " and = " + pivot);
+            // console.log("end array:", array.slice(leftIndex, rightIndex));
+
+            partition(array, leftIndex, i-1);
             partition(array, i, rightIndex);
             
         } else {
+            if (leftIndex === rightIndex) {
+                return array[leftIndex];
+            }
+
             return array.slice(leftIndex, rightIndex);
         }
     };
 
     var sorted = quickSort(array);
     console.log("quicksorted array:", sorted);
+    console.log("comparisons:", comparisons);
 
 })();
